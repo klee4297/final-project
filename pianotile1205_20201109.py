06874d2ca0285c490692f256790e95cf74c088d5
klee4297 <102543566+klee4297@users.noreply.github.com>
2023-12-12 12:05:19 +0900

    Add files via upload

--- pianotile1205_20201109.py
+++ pianotile1205_20201109.py
@@ -7,56 +7,108 @@ pygame.init()
 WIDTH, HEIGHT = 400, 600
 FPS = 60
 
-# Colors
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-
 # Initialize Pygame and create window
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Piano Tile Game")
 clock = pygame.time.Clock()
 
-# Initialize Pygame mixer
-pygame.mixer.init()
+# Load background image
+background_image = pygame.image.load("bgimg2.png")  # Replace with the path to your background image
+background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
 
-# Load sound files
-#hit_sound = pygame.mixer.Sound('hit_sound.wav')  
-pygame.mixer.music.load('voice.mp3')  #background music
+# Load player image
+player_image = pygame.image.load("anastasia1.png")  # Replace with the path to your player image
+player_image = pygame.transform.scale(player_image, (50, 70))  # Resize the player image if needed
+
+# Load tile images
+tile_images = [
+    pygame.image.load("tile1.png"),    # Replace with the path to your red tile image
+    pygame.image.load("tile2.png"),  # Replace with the path to your green tile image
+    pygame.image.load("tile3.png"),   # Replace with the path to your blue tile image
+    pygame.image.load("tile5.png"), # Replace with the path to your yellow tile image
+]
 
 # Player
 class Player(pygame.sprite.Sprite):
     def __init__(self, screen_width):
         super().__init__()
-        self.size = 50
-        self.image = pygame.Surface((self.size, self.size))
-        self.image.fill((0, 0, 0))
+        self.image = player_image  # Use the loaded player image
         self.rect = self.image.get_rect()
         self.rect.x = screen_width // 3
-        self.rect.y = HEIGHT - self.size
+        self.rect.y = HEIGHT - self.rect.height  # Adjust the y-coordinate based on the image size
         self.speed = 10
-        self.is_colliding = False  # New variable to track collision status
+        self.is_colliding = False
 
 # Tile
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, screen_width, color):
+    def __init__(self, screen_width, image, voice,initial_x, initial_y):
         super().__init__()
-        self.size = 50
-        self.image = pygame.Surface((self.size, self.size))
-        self.image.fill(color)
+        self.image = pygame.transform.scale(image, (50, 150))  # Resize the tile image if needed
         self.rect = self.image.get_rect()
-        self.rect.x = random.randint(screen_width // 6, screen_width * 5 // 6 - self.size)
-        self.rect.y = random.randint(-self.size * 2, -self.size)  # Adjusted initial y-coordinate
-        self.speed = 5
+        self.rect.x = initial_x
+        self.rect.y = initial_y
+        self.speed = 2.0
+        self.voice = voice  # Reference to the additional music
+
+    def get_scaling_factor(self, player):
+        # Calculate the scaling factor based on the overlap of player and tile
+        overlap = max(0, min(player.rect.bottom, self.rect.bottom) - max(player.rect.top, self.rect.top))
+        scaling_factor = overlap / self.rect.height
+        return scaling_factor
 
     def update(self):
         self.rect.y += self.speed  # Move the tile downward
 
-def main():
-    pygame.mixer.music.set_volume(1.0)  # Set background music volume to 0
+def starting_page():
+    start_image1 = pygame.image.load("Fimg1.png")  # Replace with the path to your starting image 1
+    start_image1 = pygame.transform.scale(start_image1, (WIDTH, HEIGHT))
 
+    start_image2 = pygame.image.load("Fimg2.png")  # Replace with the path to your starting image 2
+    start_image2 = pygame.transform.scale(start_image2, (WIDTH, HEIGHT))
+
+    nkl_image = pygame.image.load("NKL.png")  # Replace with the path to your NKL image
+    nkl_image = pygame.transform.scale(nkl_image, (93, 100))  # Adjust the size as needed
+
+    nkl_rect = nkl_image.get_rect()
+    nkl_rect.topleft = (10, HEIGHT - 110)
+
+    screen.blit(start_image1, (0, 0))  # Draw the starting image 1
+    screen.blit(nkl_image, nkl_rect.topleft)
+
+    pygame.display.flip()
+
+    waiting = True
+    while waiting:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            if event.type == pygame.KEYDOWN:
+                waiting = False
+
+        # Check if NKL is in the middle of the screen
+        if WIDTH // 3 < nkl_rect.centerx < 2 * WIDTH // 3:
+            screen.blit(start_image2, (0, 0))  # Draw the starting image 2
+        else:
+            screen.blit(start_image1, (0, 0))  # Draw the starting image 1
+
+        screen.blit(nkl_image, nkl_rect.topleft)
+        pygame.display.flip()
+
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        if pygame.mouse.get_pressed()[0]:  # Check if the left mouse button is pressed
+            if nkl_rect.collidepoint(mouse_x, mouse_y):
+                nkl_rect.x = mouse_x - nkl_rect.width // 2
+                nkl_rect.y = mouse_y - nkl_rect.height // 2
+
+    return nkl_rect
+
+    
+def main():
+    pygame.init()
+    # Show starting page and get NKL rect
+    nkl_rect = starting_page()
+    
     # Create player
     player = Player(WIDTH)
 
@@ -66,12 +118,41 @@ def main():
     all_sprites.add(player)
 
     # Sequence of tiles (0 = Red, 1 = Green, 2 = Blue, 3 = Yellow)
-    tile_sequence = [0, 1, 2, 3, 0, 1, 2, 3]  # Adjust the sequence as needed
+    tile_sequence = [0, 1, 2, 3, 1,3,2,0,3,2,3,2,1,0,2,1,2,3,0,1,1,2,0,3,2,1,2,1,0,3,1,2,1,0,2,3,1,2,0,3,2,0,1,3,2,1,3,2,3,1,0,2,1,0,1,2,3,2,0,1,2,0,1,2,0,3,1,0]  # Adjust the sequence as needed
     current_tile_index = 0
     tile_spawn_timer = 0
-    tile_spawn_delay = 60  # Adjust the delay between tile spawns
 
-    #sound_playing = False
+    # Music
+    voice = pygame.mixer.Sound('Voice.mp3')
+    voice.set_volume(0.0)
+    voice.play()
+    
+    # Initial positions for each tile
+    tile_positions = [
+        (WIDTH // 6+5, -150),      # Tile 1 position
+        (WIDTH // 2 - 55, -150),  # Tile 2 position
+        (WIDTH // 2 + 10, -150),  # Tile 3 position
+        (WIDTH * 4 // 6+10, -150),   # Tile 5 position
+    ]
+
+    # Counter variable
+    collision_count = 0
+    total_tiles_spawned = 0
+
+    # Score font
+    font = pygame.font.Font(None, 36)  # You can customize the font and size
+    
+    # Player image change threshold
+    image_change_threshold = 265
+    
+    # Background images
+    background_images = [
+        pygame.image.load("bgimg2.png"),  # Original background image
+        pygame.image.load("bgimg3.png"),  # New background image
+    ]
+    
+    # Current background index
+    current_background_index = 0
 
     # Game loop
     running = True
@@ -79,6 +160,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] and player.rect.left > WIDTH // 6:
@@ -88,36 +170,55 @@ def main():
 
         # Update timer and spawn tiles based on the sequence
         tile_spawn_timer += 1
-        if tile_spawn_timer >= tile_spawn_delay:
-            color = [RED, GREEN, BLUE, YELLOW][tile_sequence[current_tile_index]]
-            new_tile = Tile(WIDTH, color)
-            tiles.add(new_tile)
-            all_sprites.add(new_tile)
-            current_tile_index = (current_tile_index + 1) % len(tile_sequence)
-            tile_spawn_timer = 0
+        if tile_spawn_timer >= 20:  # Adjust the delay between tile spawns
+            tile_image = tile_images[tile_sequence[current_tile_index]]
+            initial_x, initial_y = tile_positions[tile_sequence[current_tile_index]]
+            new_tile = Tile(WIDTH, tile_image, voice, initial_x, initial_y)
+            
+            total_tiles_spawned += 1
+
+            if total_tiles_spawned == image_change_threshold:
+                current_background_index = 1  # Change the background image
+            elif total_tiles_spawned > image_change_threshold:
+                player.image = pygame.image.load("anastasia2.png")  # Replace with the path to the new player image
+                player.image = pygame.transform.scale(player.image, (50, 70))  # Resize the new player image if needed
+
+
+
+            overlap = pygame.sprite.spritecollideany(new_tile, tiles)
+            if not overlap:
+                tiles.add(new_tile)
+                all_sprites.add(new_tile)
+                current_tile_index = (current_tile_index + 1) % len(tile_sequence)
+                tile_spawn_timer = 0
 
         # Update sprites
         all_sprites.update()
         tiles.update()
-        
+
         # Remove tiles that are out of the screen
         tiles = pygame.sprite.Group([tile for tile in tiles if tile.rect.top < HEIGHT])
 
-
         # Check for collisions with the player
-        collisions = pygame.sprite.spritecollide(player, tiles, True) #kill block when collide
-        
         for tile in tiles:
             if pygame.sprite.collide_rect(player, tile):
                 player.is_colliding = True
-                pygame.mixer.music.set_volume(0.0)  # Set background music volume to 1
-                
+                scaling_factor = tile.get_scaling_factor(player)
+                voice.set_volume(min(1.0, scaling_factor))  # Adjust the volume based on the scaling factor
+                if not tile.is_colliding:  # Increment the counter only once per collision
+                    collision_count += 1
+                    tile.is_colliding = True
             else:
                 player.is_colliding = False
+                tile.is_colliding = False
 
-        
         # Draw everything
-        screen.fill(WHITE)
+        screen.blit(background_images[current_background_index], (0, 0))  # Draw the background image
+        
+        # Draw the score
+        score_text = font.render("Score: {}".format(collision_count), True, (255, 255, 255))  # White text
+        screen.blit(score_text, (10, 10))  # Adjust the position as needed
+
         all_sprites.draw(screen)
         tiles.draw(screen)
 
@@ -127,5 +228,4 @@ def main():
     pygame.quit()
 
 if __name__ == "__main__":
-    pygame.mixer.music.stop()  # Stop the background music when the game ends
     main()
